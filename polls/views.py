8182ebb26e5f1955669e76e61bf9844dd236f6fa
RISHAV DEWAN <rishavdewan10@gmail.com>
2025-07-02 10:42:04 +0530

    learning

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+def home(request):
+    content="<html><body><h1>Welcome to Little Lemon</h1></body></html>"
+    return HttpResponse(content)
